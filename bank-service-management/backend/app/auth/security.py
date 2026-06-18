7516c70532dc6189a8a671bdba2c95bbd55465d6
Rudra Jawale <jawalers@rknec.edu>
2026-06-19 01:28:33 +0530

    updated security with secret key

--- bank-service-management/backend/app/auth/security.py
+++ bank-service-management/backend/app/auth/security.py
@@ -1,10 +1,16 @@
 from passlib.context import CryptContext
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
 
 pwd_context = CryptContext(
     schemes=["bcrypt"],
     deprecated="auto"
 )
+SECRET_KEY = "bankservicemanagementsecretkey"
 
+ALGORITHM = "HS256"
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 def hash_password(password: str):
     return pwd_context.hash(password)
 
@@ -15,4 +21,18 @@ def verify_password(
     return pwd_context.verify(
         plain_password,
         hashed_password
+    )
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
     )
